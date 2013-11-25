7a707a49234ea043136ec9ef74a5fa911a6ea4f2
Dobrica Pavlinusic <dpavlin@rot13.org>
2013-11-25 16:11:25 +0100

    added Data::Dump perl dependency

--- bin/debian-install.sh
+++ bin/debian-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-sudo apt-get install ttf-mscorefonts-installer librsvg2-bin netpbm rlpr
+sudo apt-get install ttf-mscorefonts-installer librsvg2-bin netpbm rlpr libdata-dump-perl
 
 # install font system-wide
 sudo ln -s `pwd`/fonts/AdvCBe.ttf /usr/share/fonts/truetype/
