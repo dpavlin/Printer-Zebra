936d49f86ae85046bc0c611676dda0b57072f181
Dobrica Pavlinusic <dpavlin@rot13.org>
2015-08-26 11:12:30 +0200

    change codabar font

--- bin/debian-install.sh
+++ bin/debian-install.sh
@@ -3,4 +3,5 @@
 sudo apt-get install ttf-mscorefonts-installer librsvg2-bin netpbm rlpr libdata-dump-perl
 
 # install font system-wide
-sudo ln -s `pwd`/fonts/AdvCBe.ttf /usr/share/fonts/truetype/
+#sudo ln -s `pwd`/fonts/AdvCBe.ttf /usr/share/fonts/truetype/
+sudo ls `pwd`/fonts/*.ttf | xargs -i ln -sfv {} /usr/share/fonts/truetype/
